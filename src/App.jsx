5baa785e9d12a6573f1acc6325e9581bf6dad36e
f-koki <fukushimakoki0108@gmail.com>
2019-12-14 00:53:47 +0900

    feat: add constructor

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render() {
     return (
       <div>
         <h1>React</h1>
-        <p>This is sample component.</p>
-        <p>これはサンプルコンポーネントです。</p>
+        <p>{this.title}</p>
+        <p>{this.message}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App title="title" message="hello" />,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
