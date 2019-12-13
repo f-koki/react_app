ce58a5ad5f63b310ada233c9616438854f206749
f-koki <fukushimakoki0108@gmail.com>
2019-12-13 23:27:29 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>React</h1>
+        <p>This is sample component.</p>
+        <p>これはサンプルコンポーネントです。</p>
+      </div>
+    );
+  }
+}
+
+export default App;
